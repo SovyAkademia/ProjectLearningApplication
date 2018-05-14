8aac013c0aea2f8724090b79efdd046471faf3b7
michalnov <32653270+michalnov@users.noreply.github.com>
2018-05-14 10:09:58 +0200

    added GET method for simple String

--- JavaUserGUI/src/sk/akademiasovy/testing/webResources/HttpGet.java
+++ JavaUserGUI/src/sk/akademiasovy/testing/webResources/HttpGet.java
@@ -1,11 +1,13 @@
-package sample;
+package sk.akademiasovy.testing.webResources;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class HttpGet {
 
@@ -23,18 +25,25 @@ public class HttpGet {
         return result.toString();
     }
 
-    public String getCategories() throws Exception {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL("akademiasovy.ddns.net:3301/learn/categories");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
+    public static String executeGetSimple(String targeturl)
+    {
+        String urlString = targeturl;
+        String result = "";
+        URL url = null;
+        try {
+            url = new URL(urlString);
+            URLConnection conn = url.openConnection();
+            InputStream is = conn.getInputStream();
+            int i; char c;
+            while((i = is.read())!=-1) {
+                c = (char)i;
+                result = result + c;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        rd.close();
-        return result.toString();
+
+        return result;
     }
 
 
