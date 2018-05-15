958281d3a627eaa33054a50143c848058556140b
jozefradic <32668482+jozefradic@users.noreply.github.com>
2018-05-15 12:49:12 +0200

    update app

--- DesktopApp/src/sample/Main.java
+++ DesktopApp/src/sample/Main.java
@@ -3,19 +3,15 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
     Scene scene;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Scenes/sample.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 708, 571));
         //scene.getStylesheets().add("mainClass.css");
