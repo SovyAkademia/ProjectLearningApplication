068e385d999a8962aed59628af7dc00b7f880a06
ondrejTomco <ondrej.tomco@akademiasovy.sk>
2018-05-16 09:56:10 +0200

    getTests and testinfo routes

--- WebApp/api/controller/desktop.js
+++ WebApp/api/controller/desktop.js
@@ -9,14 +9,43 @@ exports.get_categories = (req, res, next) => {
         console.log(result[1]);
 
         res.json({
-            categories:result
+            categories: result
         })
-        
+
     });
 
 }
 
-exports.get_tests = (req,res,next) => {
-    
-    console.log(req.params);
+exports.get_tests = (req, res, next) => {
+
+    let category = req.params.category;
+
+    let getCategoryId = 'select id from categories where Name like ?';
+    let getTests = 'select * from tests where CategoryID = ?';
+
+    db.query(getCategoryId, [category], (err, categoryId) => {
+        if (err) throw err;
+        console.log(categoryId);
+
+        db.query(getTests, [categoryId[0].id], (err, tests) => {
+            if (err) throw err;
+            res.json({
+                tests
+            })
+        });
+    });
+}
+
+exports.get_test_info = (req,res,next) => {
+    let testId = req.params.id;
+    let query = 'select Count(testID) as num from test_details where testID = ?';
+
+    db.query(query,[testId],(err,questions) => {
+        if (err) throw err;
+        console.log(questions);
+
+        res.json({
+            Questions:questions
+        })
+    });
 }
--- WebApp/api/routes/desktop.js
+++ WebApp/api/routes/desktop.js
@@ -7,5 +7,6 @@ const desktopController = require('../controller/desktop');
 
 router.get('/getCategories', desktopController.get_categories);
 router.get('/getTests/:category', desktopController.get_tests);
+router.get('/testInfo/:id', desktopController.get_test_info);
 
 module.exports = router;
