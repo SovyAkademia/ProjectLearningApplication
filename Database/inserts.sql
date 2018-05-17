b92f929ce3f8e40d0d8237b273a356092daab3cf
michal.novotny <michal.novotny@akademiasovy.sk>
2018-05-17 13:21:41 +0200

    work on database inserts and finishing with app authentification

---
+++ Database/inserts.sql
@@ -0,0 +1,17 @@
+use sovylearn;
+start transaction;
+
+insert into students(Firstname, Lastname, Email, Password, year) values ('Janko', 'Mrkva','mrkva@salat.sk','$2b$10$YpYOD.zeP7T8W1t5ZTailuDP6LiKtOE0w/JgRyfB63E.B1hoKPDTC', 2000);
+insert into students(Firstname, Lastname, Email, Password, year) values ('Evka', 'Hruska','hruska@salat.sk','$2b$10$YpYOD.zeP7T8W1t5ZTailuDP6LiKtOE0w/JgRyfB63E.B1hoKPDTC', 2000);
+insert into students(Firstname, Lastname, Email, Password, year) values ('abc', 'cde','abc@cde.sk','$2b$10$xwktZI1GwKYiDWxmcBCWFOB/7wLnnSj1xxqlPkrWc8UCqBgFkY93G', 2000);
+insert into students(Firstname, Lastname, Email, Password, year) values ('123', '456','123@test.sk','$2b$10$xwktZI1GwKYiDWxmcBCWFOB/7wLnnSj1xxqlPkrWc8UCqBgFkY93G', 2000);
+
+insert into categories(CategoryName) values ('JavaScript');
+insert into categories(CategoryName) values ('SQL');
+insert into categories(CategoryName) values ('C');
+insert into categories(CategoryName) values ('Python');
+insert into categories(CategoryName) values ('Linux');
+
+
+
+commit;
