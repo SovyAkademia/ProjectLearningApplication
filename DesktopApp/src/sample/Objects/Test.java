c291b7bd6b7c912fffce17c3d2fc6ab1bb33452f
michal.novotny <michal.novotny@akademiasovy.sk>
2018-05-23 23:16:57 +0200

    added new objects for test questions and answers

--- DesktopApp/src/sample/Objects/Test.java
+++ DesktopApp/src/sample/Objects/Test.java
@@ -1,5 +1,25 @@
 package sample.Objects;
 
-public class Test {
+import java.util.ArrayList;
 
+public class Test {
+    private ArrayList<Question> testQuestions;
+    private int ID;
+
+
+    public ArrayList<Question> getTestQuestions() {
+        return testQuestions;
+    }
+
+    public void setTestQuestions(ArrayList<Question> testQuestions) {
+        this.testQuestions = testQuestions;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 }
