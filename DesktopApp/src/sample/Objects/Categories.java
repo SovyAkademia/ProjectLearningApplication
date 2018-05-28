70259344865763b554e1cbd38636a230e4d2f35c
jozefradic <32668482+jozefradic@users.noreply.github.com>
2018-05-28 12:49:55 +0200

    commit created 2 controlers,add function for category button

---
+++ DesktopApp/src/sample/Objects/Categories.java
@@ -0,0 +1,15 @@
+package sample.Objects;
+
+import java.util.ArrayList;
+
+public class Categories {
+    private ArrayList<Category> categories;
+
+    public ArrayList<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(ArrayList categories) {
+        this.categories = categories;
+    }
+}
