d21cda935bdc8145e85d3c97431077425b1e7d5d
Jakub Seliga <jakub.seliga@akademiasovy.sk>
2018-05-31 13:16:35 +0200

    add radar graph of average score

--- WebApp/api/controller/studentDetails.js
+++ WebApp/api/controller/studentDetails.js
@@ -1,10 +1,9 @@
 const db = require('../models/db');
 const _ = require('lodash');
 const flash = require('connect-flash');
-
+const dateFormat = require('dateformat');
 
 exports.show_student = (req, res, next) => {
-
     let id = req.params.id;
     let findStudent = 'select ID,FirstName, LastName from students where id like ?;';
     let findStudentTests = 'select students.ID AS "StudentID",FirstName, LastName,CategoryName,TestName, Score from students ' +
@@ -19,6 +18,17 @@ exports.show_student = (req, res, next) => {
         'INNER JOIN results ON tests.ID=results.TestID ' +
         'INNER JOIN categories ON tests.CategoryID=categories.ID WHERE results.StudentID like ?;';
     let getCategories = 'select * from categories;';
+    let averageScore = 'select avg(score) as score from results '+ 
+    'inner join tests on tests.id=results.testid '+
+    'inner join categories on tests.categoryid=categories.id where tests.categoryid = 2 '+ 
+    'union '+ 
+    'select avg(score) as score from results '+ 
+    'inner join tests on tests.id=results.testid '+
+    'inner join categories on tests.categoryid=categories.id where tests.categoryid = 1 '+
+    'union '+
+    'select avg(score) as score from results '+
+    'inner join tests on tests.id=results.testid '+
+    'inner join categories on tests.categoryid=categories.id where tests.categoryid = 3;';
 
     db.query(findStudent,[id],(err,studentInfo)=>{
         if (err) return next(err); 
@@ -37,20 +47,34 @@ exports.show_student = (req, res, next) => {
                     testArr.push(test);
                     scoreArr.push(score);
                 }
-                db.query(getCategories, (err, categories) => {
-                    if (err) {
-                        return res.render('studentDetails', {
-                            error: err
-                        });
+                db.query(averageScore, (err,scoreRadar)=>{
+                    if (err) return next(err);
+                    let scoreRadarArr = [];
+                    for (index = 0; index < 3; index++) {
+                        if (scoreRadar[index].score != null) {
+                            let element = (scoreRadar[index].score);
+                            scoreRadarArr.push(element);            
+                        }else{
+                            scoreRadarArr.push(0);            
+                        }
                     }
-                    res.render('studentDetails', {
-                        categories,
-                        student,
-                        testArr,
-                        scoreArr,
-                        studentInfo
+                    console.log(scoreRadarArr)
+                    db.query(getCategories, (err, categories) => {
+                        if (err) {
+                            return res.render('studentDetails', {
+                                error: err
+                            });
+                        }
+                        res.render('studentDetails', {
+                            categories,
+                            student,
+                            testArr,
+                            scoreArr,
+                            studentInfo,
+                            scoreRadarArr
+                        });
                     });
-                });
+                });                
             });
         });
     });
@@ -76,6 +100,9 @@ exports.tests_in_category = (req, res, next) => {
         'INNER JOIN categories ON tests.CategoryID=categories.ID ' +
         'WHERE results.StudentID like ? and categoryid like ?;';
     let getCategories = 'select * from categories';
+    let averageScore = 'select avg(score) as scoreHTML from results '+
+    'inner join tests on tests.id=results.testid '+
+    'inner join categories on tests.categoryid=categories.id where categories like ?;';
 
     if (categoryId == 'all') {
         return res.redirect('/students/' + studentID);
@@ -89,7 +116,6 @@ exports.tests_in_category = (req, res, next) => {
             let num = count[0].Count;
             if (num != 0 || student !=null){ 
                 db.query(findStudentResults, [studentID, categoryId], (err, testScore) => {
-                    console.log(testScore);
                     if (err) return next(err);
                     let testArr = [];
                     let scoreArr = [];
@@ -110,7 +136,7 @@ exports.tests_in_category = (req, res, next) => {
                             student,
                             testArr,
                             scoreArr,
-                            studentInfo
+                            studentInfo                            
                         });
                     });
                 });
